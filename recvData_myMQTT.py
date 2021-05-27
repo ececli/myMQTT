cc7da9084403b0e5db6e3c6fee1101a2576ab585
Chang <64391700+ececli@users.noreply.github.com>
2021-05-27 12:41:44 -0400

    Update recvData_myMQTT.py

--- recvData_myMQTT.py
+++ recvData_myMQTT.py
@@ -1,20 +1,22 @@
 from myMQTT_Class import myMQTT
 
 
-broker_address="192.168.1.210"
+broker_address="192.168.1.210" # broker address
 topic1 = "ranging/delay/t3t2_delay_ms"
 topic2 = "ranging/delay/t3t2_delay_count"
-mqttc = myMQTT(broker_address, "RPi2")
-mqttc.registerTopic(topic1)
+mqttc = myMQTT(broker_address, "RPi2") 
+mqttc.registerTopic(topic1) # subscribe the topic so that it could receive data for this topic
 mqttc.registerTopic(topic2)
 
 
 while True:
-    if mqttc.checkTopicDataLength(topic1)==10 and mqttc.checkTopicDataLength(topic2)==10:
+    # stop when the amount of data is satisfied
+    # The sender sends 10 int values for each topic
+    if mqttc.checkTopicDataLength(topic1)==10 and mqttc.checkTopicDataLength(topic2)==10: 
         mqttc.closeClient()
         break
 
-
+# export the data to the local variables
 a = mqttc.readTopicData(topic1)
 b = mqttc.readTopicData(topic2)
 print(a)
